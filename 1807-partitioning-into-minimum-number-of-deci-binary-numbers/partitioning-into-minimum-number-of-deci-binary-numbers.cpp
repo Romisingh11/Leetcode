5ad7208ef0ea6e39c632ba6ba5f16c2dbdba0ee6
Romisingh11 <romisingh965@gmail.com>
2026-03-01 23:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (53.96%) - LeetSync

---
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxDigit = 0;
+        
+        for(char c : n) {
+            maxDigit = max(maxDigit, c - '0');
+            if(maxDigit == 9) return 9; // early exit (max possible)
+        }
+        
+        return maxDigit;
+    }
+};
