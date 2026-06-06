7481439b379cc2f12b8f72db763e6d09b9836973
Romisingh11 <romisingh965@gmail.com>
2026-06-06 12:07:16 +0530

    Time: 55 ms (82.50%) | Memory: 46.8 MB (58.75%) - LeetSync

--- 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.java
+++ 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.java
@@ -1,81 +1,92 @@
 class Solution {
 
     static class Node {
-        long cnt;     // number of valid numbers
-        long wavy;    // total waviness sum
+        long cnt;   // number of numbers
+        long wav;   // total waviness
 
-        Node(long cnt, long wavy) {
+        Node(long cnt, long wav) {
             this.cnt = cnt;
-            this.wavy = wavy;
+            this.wav = wav;
         }
     }
 
     private char[] digits;
-    private Node[][][][][] memo;
+    private Node[][][][] memo;
 
     public long totalWaviness(long num1, long num2) {
         return solve(num2) - solve(num1 - 1);
     }
 
     private long solve(long n) {
-        if (n < 0) return 0;
+        if (n <= 0) return 0;
 
         digits = Long.toString(n).toCharArray();
 
-        int m = digits.length;
-        memo = new Node[m + 1][3][11][11][1];
+        int len = digits.length;
+        memo = new Node[len][11][11][2];
 
-        return dfs(0, true, 0, -1, -1).wavy;
+        return dfs(0, -1, -1, false, true).wav;
     }
 
-    private Node dfs(int pos, boolean tight, int lenState, int prev2, int prev1) {
+    private Node dfs(int pos, int prev2, int prev1,
+                     boolean started, boolean tight) {
+
         if (pos == digits.length) {
             return new Node(1, 0);
         }
 
         if (!tight) {
-            Node saved = memo[pos][lenState][prev2 + 1][prev1 + 1][0];
+            Node saved = memo[pos][prev2 + 1][prev1 + 1][started ? 1 : 0];
             if (saved != null) return saved;
         }
 
         int limit = tight ? digits[pos] - '0' : 9;
 
         long totalCnt = 0;
-        long totalWavy = 0;
+        long totalWav = 0;
 
         for (int d = 0; d <= limit; d++) {
+
             boolean nextTight = tight && (d == limit);
 
-            if (lenState == 0) {
-                // Still skipping leading zeros
-                if (d == 0) {
-                    Node child = dfs(pos + 1, nextTight, 0, -1, -1);
-                    totalCnt += child.cnt;
-                    totalWavy += child.wavy;
-                } else {
-                    Node child = dfs(pos + 1, nextTight, 1, -1, d);
-                    totalCnt += child.cnt;
-                    totalWavy += child.wavy;
-                }
-            } else if (lenState == 1) {
-                Node child = dfs(pos + 1, nextTight, 2, prev1, d);
+            if (!started && d == 0) {
+                Node child = dfs(pos + 1, -1, -1, false, nextTight);
+
                 totalCnt += child.cnt;
-                totalWavy += child.wavy;
+                totalWav += child.wav;
             } else {
-                int add = ((prev1 > prev2 && prev1 > d) ||
-                           (prev1 < prev2 && prev1 < d)) ? 1 : 0;
 
-                Node child = dfs(pos + 1, nextTight, 2, prev1, d);
+                boolean nextStarted = true;
+
+                int add = 0;
+
+                if (prev2 != -1) {
+                    if ((prev1 > prev2 && prev1 > d) ||
+                        (prev1 < prev2 && prev1 < d)) {
+                        add = 1;
+                    }
+                }
+
+                int newPrev2 = prev1;
+                int newPrev1 = d;
+
+                Node child = dfs(
+                        pos + 1,
+                        newPrev2,
+                        newPrev1,
+                        nextStarted,
+                        nextTight
+                );
 
                 totalCnt += child.cnt;
-                totalWavy += child.wavy + add * child.cnt;
+                totalWav += child.wav + add * child.cnt;
             }
         }
 
-        Node ans = new Node(totalCnt, totalWavy);
+        Node ans = new Node(totalCnt, totalWav);
 
         if (!tight) {
-            memo[pos][lenState][prev2 + 1][prev1 + 1][0] = ans;
+            memo[pos][prev2 + 1][prev1 + 1][started ? 1 : 0] = ans;
         }
 
         return ans;
